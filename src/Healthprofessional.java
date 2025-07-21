32bb33b12612813201681aa3ce12f1a03a29d212
Niven215 <nivenmugambi@gmail.com>
2025-07-21 08:10:39 +0800

    Update Healthprofessional.java
    created a base class

--- src/Healthprofessional.java
+++ src/Healthprofessional.java
@@ -1,2 +1,21 @@
-public class Healthprofessional {
+public class Healthprofessional {public class HealthProfessional {
+    protected int id;
+    protected String name;
+    protected String department; // Additional basic info
+
+    public HealthProfessional() {}
+
+    public HealthProfessional(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    public void printDetails() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+    }
+}
+
 }

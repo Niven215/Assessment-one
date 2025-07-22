e9b58219300385d7f7668c82c0423e395a697ea4
Niven215 <nivenmugambi@gmail.com>
2025-07-22 16:12:58 +0800

    toString method

--- src/Healthprofessional.java
+++ src/Healthprofessional.java
@@ -1,21 +1,24 @@
-public class Healthprofessional {public class HealthProfessional {
+public abstract class Healthprofessional {
+    // null con
     protected int id;
     protected String name;
     protected String department; // Additional basic info
 
-    public HealthProfessional() {}
+    public Healthprofessional() {}
 
-    public HealthProfessional(int id, String name, String department) {
+    public Healthprofessional(int id, String name, String department) {
         this.id = id;
         this.name = name;
         this.department = department;
     }
-
-    public void printDetails() {
-        System.out.println("ID: " + id);
-        System.out.println("Name: " + name);
-        System.out.println("Department: " + department);
-    }
+// need to convert this method into toString
+@Override
+public String toString() {
+    return "ID: " + id + "\n"
+            + "Name: " + name + "\n"
+            + "Department: " + department;
 }
 
 }
+
+

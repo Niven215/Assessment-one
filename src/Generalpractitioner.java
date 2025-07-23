594df8372c68f8d42474266a05accfe067c92f19
Niven215 <nivenmugambi@gmail.com>
2025-07-23 18:31:49 +0800

    Created a general practitioner class

--- src/Generalpractitioner.java
+++ src/Generalpractitioner.java
@@ -1,2 +1,17 @@
-public class Generalpractitioner {
+public class Generalpractitioner extends Healthprofessional {
+    private String clinicRoom;
+
+    public Generalpractitioner() {}
+
+    public Generalpractitioner(int id, String name, String department, String clinicRoom) {
+        super(id, name, department);
+        this.clinicRoom = clinicRoom;
+    }
+
+    @Override
+    public String toString() {
+        return "Health Professional Type: General Practitioner\n"
+                + super.toString() + "\n"
+                + "Clinic Room: " + clinicRoom;
+    }
 }
